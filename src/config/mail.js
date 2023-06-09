725f46e1e983a90be259d86c3b8700cca7fe70bd
Davibarreto11 <daviarturdesa686@gmail.com>
2023-06-09 15:57:07 -0300

    configuration nodemailer for emails templates

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'sandbox.smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '14c932c5ac8a62',
+    pass: '2da8b4810e6bc8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
