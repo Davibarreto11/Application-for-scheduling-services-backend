597f1d943bd55f4a874212384d873d23c1042a13
Davibarreto11 <daviarturdesa686@gmail.com>
2023-07-18 11:04:50 -0300

    node.js`s security

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -15,10 +19,27 @@ import './database';
 const app = express();
 Sentry.init(sentryConfig);
 app.use(Sentry.Handlers.requestHandler());
-app.use(cors());
+app.use(helmet());
+app.use(cors({
+  origin: process.env.FRONT_URL,
+}));
 
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'temp', 'uploads')));
+
+if (process.env.NODE_ENV !== 'development') {
+  app.use(new RateLimit({
+    store: new RateLimitRedis({
+      client: redis.createClient({
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+      }),
+    }),
+    windowMs: 1000 * 60 * 15,
+    max: 100,
+  }));
+}
+
 app.use(routes);
 app.use(Sentry.Handlers.errorHandler());
 
